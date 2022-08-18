62948dd7c827d451a5cef4ddf5ed247a4e1c68d9
Ish <ngetichishmael786@gmail.com>
2022-08-18 23:32:03 +0300

    Fixed Sales Orders and Vansales

--- app/Http/Controllers/Api/CheckingSaleOrderController.php
+++ app/Http/Controllers/Api/CheckingSaleOrderController.php
@@ -9,85 +9,252 @@ use App\Models\Cart;
 use App\Models\Orders as Order;
 use Illuminate\Support\Facades\DB;
 
+
 class CheckingSaleOrderController extends Controller
 {
+
+    public function amount(Request $request,$checkinCode){
+        $checkin = checkin::where('code', $checkinCode)->first();
+        $request = $request->all();
+        $total=0;
+        array_pop($request);
+        foreach ($request as $value){
+            $product = product_information::join('product_price',
+            'product_price.productID', '=', 'product_information.id')
+           ->where('product_information.id', $value["productID"])
+           ->where('product_information.business_code', $checkin->business_code)
+           ->first();
+           $total_amount = $value["qty"] * $product->selling_price;
+           $total+=$total_amount;
+        }
+        return $total;
+        }
+
     //Start Vansales
     public function VanSales(Request $request, $checkinCode)
     {
+        $amountRequest=$request;
         $checkin = checkin::where('code', $checkinCode)->first();
         $user_code = $request->user()->user_code;
-        $business_code = $request->user()->business_code;
-        $random = Str::random(8);
-        $total=0;
-        $quanty=0;
         $request = $request->all();
         array_pop($request);
-         foreach ($request as $value) {
-            //  $product = product_information::join(
-            //      'product_price',
-            //      'product_price.productID',
-            //      '=',
-            //      'product_information.id'
-            //  )
-            //      ->where('product_information.id', $value["productID"])
-            //      ->where('product_information.business_code', $checkin->business_code)
-            //      ->first();
-                 $product = DB::table('product_information')
-                 ->join('product_price', 'product_information.id', '=', 'product_price.productID')
-                 ->where('product_information.id', $value['productID'])
-                 ->where('product_information.business_code', $checkin->business_code)
-                 ->select('product_information.*', 'product_price.*')
-                 ->limit(1)
-                 ->get();
-             $random = Str::random(8);
-             $checkInCart = Cart::where('checkin_code', $checkinCode)->where('productID', $value['productID'])->count();
-             if ($checkInCart > 0) {
-                 $cart = Cart::where('checkin_code', $checkinCode)->where('productID', $value['productID'])->first();
-                 $cart->qty = $value["qty"];
-                 $cart->price = $product->selling_price;
-                 $cart->amount = $value["qty"] * $product->selling_price;
-                 $cart->total_amount = $value["qty"] * $product->selling_price;
-                 $cart->userID = $user_code;
-                 $cart->save();
-             } else {
-                 $cart = new Cart;
-                 $cart->productID = $value["productID"];
-                 $cart->product_name = $product->product_name;
-                 $cart->qty = $value["qty"];
-                 $cart->price = $product->selling_price;
-                 $cart->amount = $value["qty"] * $product->selling_price;
-                 $cart->userID = $user_code;
-                 $cart->customer_account = $checkin->account_number;
-                 $cart->total_amount = $value["qty"] * $product->selling_price;
-                 $cart->checkin_code = $checkinCode;
-                 $cart->save();
-             }
-                Order::updateOrCreate(
-                    [
-                        'order_code' => $random, 
-                        'user_code' =>  $user_code,
-                        'customerID'=>$checkin->customer_id,
-                        'checkin_code'=> $checkinCode,
-                        'order_type' => 'Van Sale'
-                    ],
-                    [
-                        'price_total' =>$total,
-                        'balance' =>$total,
-                        'order_status'=>'Pending Payment',
-                        'payment_status'=>'Pending Payment',
-                        'qty'=>$quanty,
-                        'created_at'=>now(),
-                        'updated_at'=>now(),
-                        'business_code'=> $business_code
-                    ]
-                );
+        foreach ($request as $value) {
+            $product = product_information::join(
+                'product_price',
+                'product_price.productID',
+                '=',
+                'product_information.id'
+            )
+                ->where('product_information.id', $value["productID"])
+                ->where('product_information.business_code', $checkin->business_code)
+                ->first();
+            $random = Str::random(8);
+            $checkInCart = Cart::where('checkin_code', $checkinCode)->where('productID', $value["productID"])->count();
+            if ($checkInCart > 0) {
+                $cart = Cart::where('checkin_code', $checkinCode)->where('productID', $value["productID"])->first();
+                $cart->qty = $value["qty"];
+                $cart->price = $product->selling_price;
+                $cart->amount = $value["qty"] * $product->selling_price;
+                $cart->total_amount = $value["qty"] * $product->selling_price;
+                $cart->userID = $user_code;
+                $cart->save();
+            } else {
+                $cart = new Cart;
+                $cart->productID = $value["productID"];
+                $cart->product_name = $product->product_name;
+                $cart->qty = $value["qty"];
+                $cart->price = $product->selling_price;
+                $cart->amount = $value["qty"] * $product->selling_price;
+                $cart->userID = $user_code;
+                $cart->customer_account = $checkin->account_number;
+                $cart->total_amount = $value["qty"] * $product->selling_price;
+                $cart->checkin_code = $checkinCode;
+                $cart->save();
             }
-            return response()->json([
-                "success" => true,
-                "message" => "Product added to order",
-                "data"    => $checkin
-            ]);
         }
+
+        DB::insert(
+            'INSERT INTO `orders`(
+            `order_code`,
+            `user_code`,
+            `customerID`,
+            `price_total`,
+            `balance`,
+            `order_status`,
+            `payment_status`,
+            `qty`,
+            `checkin_code`,
+            `order_type`,
+            `delivery_date`,
+            `business_code`,
+            `created_at`)
+        VALUES (?,?,?, ?,?, ?,?, ?,?, ?,?,?,?)',
+
+            [
+                $random, 
+                $user_code, 
+                $checkin->customer_id,
+                $this->amount($amountRequest, $checkinCode),
+                $this->amount($amountRequest, $checkinCode),
+                'Pending Delivery', 
+                'Pending Payment', 
+                $value["qty"],
+                $checkinCode, 
+                'Van Sale', 
+                now(), 
+                $checkin->business_code,
+                now()
+            ]
+        );
+        DB::insert(
+            'INSERT INTO `order_items`(
+            `order_code`,
+            `productID`,
+            `product_name`,
+            `quantity`,
+            `sub_total`,
+            `total_amount`,
+            `selling_price`,
+            `discount`,
+            `taxrate`,
+            `taxvalue`,
+            `created_at`,
+            `updated_at`
+        )
+        VALUES (?,?,?, ?,?, ?,?, ?,?, ?,?,?)',
+            [
+                $random, 
+                $value["productID"], 
+                $product->product_name, 
+                $value["qty"],
+                $value["qty"] * $product->selling_price, 
+                $value["qty"] * $product->selling_price,
+                0, 
+                0, 
+                0, 
+                0, 
+                now(), 
+                now()
+            ]
+        );
+        return response()->json([
+            "success" => true,
+            "message" => "Product added to order",
+            "data"    => $checkin
+        ]);
+    }
+
     //End of Vansales 
 
+
+    // Beginning of NewSales
+    public function NewSales(Request $request, $checkinCode)
+    {
+        $amountRequest=$request;
+        $checkin = checkin::where('code', $checkinCode)->first();
+        $user_code = $request->user()->user_code;
+        $request = $request->all();
+        array_pop($request);
+        foreach ($request as $value) {
+            $product = product_information::join(
+                'product_price',
+                'product_price.productID',
+                '=',
+                'product_information.id'
+            )
+                ->where('product_information.id', $value["productID"])
+                ->where('product_information.business_code', $checkin->business_code)
+                ->first();
+            $checkInCart = Cart::where('checkin_code', $checkinCode)->where('productID', $value["productID"])->count();
+            if ($checkInCart > 0) {
+                $cart = Cart::where('checkin_code', $checkinCode)->where('productID', $value["productID"])->first();
+                $cart->qty = $value["qty"];
+                $cart->price = $product->selling_price;
+                $cart->amount = $value["qty"] * $product->selling_price;
+                $cart->total_amount = $value["qty"] * $product->selling_price;
+                $cart->userID = $user_code;
+                $cart->save();
+            } else {
+                $cart = new Cart;
+                $cart->productID = $value["productID"];
+                $cart->product_name = $product->product_name;
+                $cart->qty = $value["qty"];
+                $cart->price = $product->selling_price;
+                $cart->amount = $value["qty"] * $product->selling_price;
+                $cart->userID = $user_code;
+                $cart->customer_account = $checkin->account_number;
+                $cart->total_amount = $value["qty"] * $product->selling_price;
+                $cart->checkin_code = $checkinCode;
+                $cart->save();
+            }
+        }
+        $random = Str::random(8);
+        DB::insert(
+            'INSERT INTO `orders`(
+            `order_code`,
+            `user_code`,
+            `customerID`,
+            `price_total`,
+            `balance`,
+            `order_status`,
+            `payment_status`,
+            `qty`,
+            `checkin_code`,
+            `order_type`,
+            `delivery_date`,
+            `business_code`,
+            `created_at`
+            )
+
+        VALUES (?,?,?, ?,?, ?,?, ?,?, ?,?,?,?)',
+
+            [
+                $random,
+                $user_code,
+                $checkin->customer_id,
+                $this->amount($amountRequest, $checkinCode),
+                $this->amount($amountRequest, $checkinCode),
+                'Pending Delivery',
+                'Pending Payment',
+                $value["qty"],
+                $checkinCode,
+                'Pre Order',
+                now(),
+                $checkin->business_code,
+                now()
+            ]
+        );
+
+        DB::insert(
+            'INSERT INTO `order_items`(
+        `order_code`,
+        `productID`,
+        `product_name`,
+        `quantity`,
+        `sub_total`,
+        `total_amount`,
+        `selling_price`,
+        `discount`,
+        `taxrate`,
+        `taxvalue`,
+        `created_at`,
+        `updated_at`)
+
+        VALUES (?,?,?, ?,?, ?,?, ?,?, ?,?,?)',
+            [
+                $random,
+                $value["productID"], 
+                $product->product_name, 
+                $value["qty"],
+                $value["qty"] * $product->selling_price, 
+                $value["qty"] * $product->selling_price,
+                0, 0, 0, 0, now(), now()
+            ]
+        );
+        return response()->json([
+            "success" => true,
+            "message" => "Product added to order",
+            "data"    => $checkin
+        ]);
     }
+}
