0f7cd31839d3b8ac81de0e4abdf5ef54b31e2c5b
Ish <ngetichishmael786@gmail.com>
2022-09-08 19:40:59 +0300

    updates

--- app/Http/Controllers/Api/ReconcilationController.php
+++ app/Http/Controllers/Api/ReconcilationController.php
@@ -11,31 +11,31 @@ class ReconcilationController extends Controller
     public function index(Request $request)
     {
         $user_id = $request->user()->id;
-        $mpesa= DB::select('SELECT
+        $mpesa = DB::select('SELECT
         SUM(`amount`)
                 FROM
                     `order_payments`
                 WHERE
-                    `order_payments`.`payment_method` =? AND `order_payments`.`user_id`=?',['PaymentMethods.Mpesa',$user_id]);
-        $cash= DB::select('SELECT
+                    `order_payments`.`payment_method` =? AND `order_payments`.`user_id`=?', ['PaymentMethods.Mpesa', $user_id]);
+        $cash = DB::select('SELECT
         SUM(`amount`)
                 FROM
                     `order_payments`
                 WHERE
-                    `order_payments`.`payment_method` =? AND `order_payments`.`user_id`=?',['PaymentMethods.Cash',$user_id]);
-        $cheque= DB::select('SELECT
+                    `order_payments`.`payment_method` =? AND `order_payments`.`user_id`=?', ['PaymentMethods.Cash', $user_id]);
+        $cheque = DB::select('SELECT
         SUM(`amount`)
                 FROM
                     `order_payments`
                 WHERE
-                    `order_payments`.`payment_method` =? AND `order_payments`.`user_id`=?',['PaymentMethods.Cheque',$user_id]);
+                    `order_payments`.`payment_method` =? AND `order_payments`.`user_id`=?', ['PaymentMethods.Cheque', $user_id]);
 
-return response()->json([
-    "success" => true,
-    "message" => "Total Amount Expected",
-    "Mpesa" => $mpesa,
-    "Cash" => $cash,
-    "Cheque" => $cheque
-]);
+        return response()->json([
+            "success" => true,
+            "message" => "Total Amount Expected",
+            "Mpesa" => $mpesa,
+            "Cash" => $cash,
+            "Cheque" => $cheque
+        ]);
     }
 }
