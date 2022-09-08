3064e7770253298e58b9d98fa2d7e37a58f1f7c1
Ish <ngetichishmael786@gmail.com>
2022-09-08 17:35:50 +0300

    Updates to forms

--- app/Http/Controllers/Api/surveyController.php
+++ app/Http/Controllers/Api/surveyController.php
@@ -24,47 +24,4 @@ class surveyController extends Controller
       //"Data" =>  $this->data()
     ]);
   }
-  public function data()
-  {
-    $arrayQuestion = array();
-    $typeOnline = "online";
-    $querySurvey = DB::select('SELECT `code` FROM `survey` where type=?', [$typeOnline]);
-    foreach ($querySurvey as $value) {
-      $arrayQuestionData = [];
-      $query = DB::select('SELECT
-        `survey_questions`.`id`,
-        `survey_questions`.`survey_code`,
-        `survey_question_types`.`name` AS Type,
-        `survey_questions`.`question`
-          FROM
-              `survey_questions`
-          INNER JOIN `survey_question_types` ON `survey_questions`.`type` = `survey_question_types`.`id`
-          WHERE
-              `survey_questions`. `survey_code` = ?
-              ', [$value->code]);
-      if (!(empty($query))) {
-        foreach ($query as $queryID) {
-          array_push($arrayQuestionData, $this->arrayQuestions($queryID->id));
-        }
-        array_push($arrayQuestion, $query, $arrayQuestionData);
-      }
-    }
-    info($arrayQuestion);
-    return $arrayQuestion;
-  }
-
-  public function arrayQuestions($id)
-  {
-    $questions = DB::select('SELECT
-        `id`,
-        `survey_code`,
-        `questionID`,
-        `option_a`,
-        `option_b`,
-        `option_c`,
-        `option_d`
-    FROM
-        `survey_question_answers` WHERE `questionID` =?', [$id]);
-    return $questions;
-  }
 }
