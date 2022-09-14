942c97ac6788fea396a145b648c67b67da672baf
ngetichishmael <ngetichishmael786@gmail.com>
2022-09-14 20:37:01 +0300

    Survey Responses

---
+++ app/Http/Controllers/Api/SurveryAnswersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\SurveyResponses as Response;
+use Illuminate\Http\Request;
+
+class SurveryAnswersController extends Controller
+{
+    public function index(Request $request)
+    {
+        info($request);
+        $data= $request->all();
+        $response = new Response();
+        $response->survey_code=$data()->survey_code;
+        $response->question_code=$data()->question_code;
+        $response->customer_id=$data()->customer_id;
+        $response->answer=$data()->answer;
+        $response->reason=$data()->reason;
+
+        return response()->json([
+            "success" => true,
+            "Message" => "Data sent Successfully",
+            "Status" => 200
+        ]);
+
+    }
+}
---
+++ database/migrations/2022_09_14_171732_create_survey_responses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveyResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_responses', function (Blueprint $table) {
+            $table->id();
+            $table->string('survey_code');
+            $table->string('question_code');
+            $table->bigInteger('customer_id');
+            $table->string('answer');
+            $table->text('reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_responses');
+    }
+}
