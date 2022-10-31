0376a7da18e7ea67e082ccfa03cc8da4cc034ba3
ngetichishmael <ngetichishmael786@gmail.com>
2022-10-31 14:16:36 +0300

    update to reports

--- app/Http/Controllers/Api/ReportsController.php
+++ app/Http/Controllers/Api/ReportsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Orders;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportsController extends Controller
@@ -20,42 +21,42 @@ class ReportsController extends Controller
 
       // $countYear = ModelsCustomers::whereYear('updated_at', now())
       //    ->count();
-      $countOrdersToday=Orders::where('user_code',$user_code)
-                           ->whereBetween('created_at', today())
-                           ->where('order_status','DELIVERED')
-                           ->count();
-      $countOrdersWeekly=Orders::where('user_code',$user_code)
-                           ->whereWeek('created_at', today())
-                           ->where('order_status','DELIVERED')
-                           ->count();
-      $countOrdersMonthly=Orders::where('user_code',$user_code)
-                           ->whereMonth('created_at', today())
-                           ->where('order_status','DELIVERED')
-                           ->count();
-      $totalSalesToday=Orders::where('user_code',$user_code)
-                           ->whereBetween('created_at', today())
-                           ->where('order_status','DELIVERED')
-                           ->sum('price_total');
-      $totalSalesWeekly=Orders::where('user_code',$user_code)
-                           ->whereWeek('created_at', today())
-                           ->where('order_status','DELIVERED')
-                           ->sum('price_total');
-      $totalSalesMonthly=Orders::where('user_code',$user_code)
-                           ->whereMonth('created_at', today())
-                           ->where('order_status','DELIVERED')
-                           ->sum('price_total');
+      $countOrdersToday = Orders::where('user_code', $user_code)
+         ->whereDate('created_at', '=', Carbon::today()->toDateString())
+         ->where('order_status', 'DELIVERED')
+         ->count();
+      $countOrdersWeekly = Orders::where('user_code', $user_code)
+         ->whereWeek('created_at', today())
+         ->where('order_status', 'DELIVERED')
+         ->count();
+      $countOrdersMonthly = Orders::where('user_code', $user_code)
+         ->whereMonth('created_at', today())
+         ->where('order_status', 'DELIVERED')
+         ->count();
+      $totalSalesToday = Orders::where('user_code', $user_code)
+         ->whereDate('created_at', '=', Carbon::today()->toDateString())
+         ->where('order_status', 'DELIVERED')
+         ->sum('price_total');
+      $totalSalesWeekly = Orders::where('user_code', $user_code)
+         ->whereWeek('created_at', today())
+         ->where('order_status', 'DELIVERED')
+         ->sum('price_total');
+      $totalSalesMonthly = Orders::where('user_code', $user_code)
+         ->whereMonth('created_at', today())
+         ->where('order_status', 'DELIVERED')
+         ->sum('price_total');
 
-         return response()->json([
-            "success" => true,
-            "status" => 200,
-            "Data" => [
-               'countOrdersToday'=>$countOrdersToday,
-               'countOrdersWeekly'=>$countOrdersWeekly,
-               'countOrdersMonthly'=>$countOrdersMonthly,
-               'totalSalesToday'=>$totalSalesToday,
-               'totalSalesWeekly'=>$totalSalesWeekly,
-               'totalSalesMonthly'=>$totalSalesMonthly,
-            ]
-         ]);
+      return response()->json([
+         "success" => true,
+         "status" => 200,
+         "Data" => [
+            'countOrdersToday' => $countOrdersToday,
+            'countOrdersWeekly' => $countOrdersWeekly,
+            'countOrdersMonthly' => $countOrdersMonthly,
+            'totalSalesToday' => $totalSalesToday,
+            'totalSalesWeekly' => $totalSalesWeekly,
+            'totalSalesMonthly' => $totalSalesMonthly,
+         ]
+      ]);
    }
 }
