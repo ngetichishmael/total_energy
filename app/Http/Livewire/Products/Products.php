352498ead3e2d5f4db649ba1d9e6d36942014b8f
ngetichishmael <ngetichishmael786@gmail.com>
2022-11-11 13:26:43 +0300

    updates to product inventory

--- app/Http/Livewire/Products/Products.php
+++ app/Http/Livewire/Products/Products.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Livewire\Products;
+
 use App\Models\products\product_information;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth as FacadesAuth;
@@ -19,16 +20,23 @@ class Products extends Component
    {
 
       $products =  product_information::search($this->search)
-                     ->join('business','business.business_code','=','product_information.business_code')
-                     ->join('product_inventory','product_inventory.productID','=','product_information.id')
-                     ->join('product_price','product_price.productID','=','product_information.id')
-                     ->whereNull('parentID')
-                     ->where('default_inventory','Yes')
-                     ->where('default_price','Yes')
-                     ->where('product_information.business_code', FacadesAuth::user()->business_code)
-                     ->select('product_information.id as proID','product_information.created_at as date','product_price.selling_price as price','product_information.product_name as product_name','product_inventory.current_stock as stock','product_information.created_at as date','product_information.business_code as business_code','sku_code','brand')
-                     ->orderBy($this->orderBy,$this->orderAsc ? 'desc' : 'asc')
-                     ->paginate($this->perPage);
+         ->join('business', 'business.business_code', '=', 'product_information.business_code')
+         ->join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
+         ->join('product_price', 'product_price.productID', '=', 'product_information.id')
+         ->where('product_information.business_code', FacadesAuth::user()->business_code)
+         ->select(
+            'product_information.id as proID',
+            'product_information.created_at as date',
+            'product_price.selling_price as price',
+            'product_information.product_name as product_name',
+            'product_inventory.current_stock as stock',
+            'product_information.created_at as date',
+            'product_information.business_code as business_code',
+            'sku_code',
+            'brand'
+         )
+         ->orderBy($this->orderBy, $this->orderAsc ? 'desc' : 'asc')
+         ->paginate($this->perPage);
 
       return view('livewire.products.products', compact('products'));
    }
