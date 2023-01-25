8185fb5e607741a1f763c2ba449e587c0dfb70b4
ngetichishmael <ngetichishmael786@gmail.com>
2023-01-25 06:17:17 +0300

    updates to outlet seeders

--- database/seeders/OutletSeeder.php
+++ database/seeders/OutletSeeder.php
@@ -11,15 +11,25 @@ class OutletSeeder extends Seeder
    /**
     * Run the database seeds.
     *
+    Spare Part Shops
+Garages
+Independent Network
+Wholesalers
+Motorcycle dealer
+Transporter
+Hardware Store
+Agri Consumer
     * @return void
     */
    public $data = [
-      "Qx4FstqLJfHwf3WA" => "Supermarket",
-      "Qx4FstqLJfHwf3WA" => "WholeSales",
-      "Qx4FstqLJfHwf3WA" => "Kiosk",
-      "Qx4FstqLJfHwf3WA" => "Mini Supermarket",
-      "Qx4FstqLJfHwf3WA" => "Hardware",
-      "Qx4FstqLJfHwf3WA" => "Retail Shop",
+      "Qx4FstqLJfHwf3WA" => "Independent Network",
+      "Qx4FstqLJfHwf3WA" => "Spare Part Shops",
+      "Qx4FstqLJfHwf3WA" => "Garages",
+      "Qx4FstqLJfHwf3WA" => "Wholesalers",
+      "Qx4FstqLJfHwf3WA" => "Motorcycle dealer",
+      "Qx4FstqLJfHwf3WA" => "Transporter",
+      "Qx4FstqLJfHwf3WA" => "Hardware Store",
+      "Qx4FstqLJfHwf3WA" => "Agri Consumer",
    ];
    public function run()
    {
