3b522b1225bb1a7c98a72039e9e85b94b5eba997
ngetichishmael <ngetichishmael786@gmail.com>
2023-02-02 11:57:00 +0300

    reverting back to vansales

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
@@ -29,15 +30,14 @@ class AuthController extends Controller
          "access_token": "254|33icFsR8uIOF1KsOeaJ114ntrU8adIX7gitwAveK",
          "token_type": "Bearer"
       }
-   **/
+    **/
    public function userLogin(Request $request)
    {
 
       //(!Auth::attempt(['email' => $request->email, 'password' => $request->password], true))
-      if(!FacadesAuth::attempt(['email' => $request->email, 'password' => $request->password], true))
-      {
+      if (!FacadesAuth::attempt(['phone_number' => $request->email, 'password' => $request->password], true)) {
          return response()
-               ->json(['message' => 'Unauthorized'], 401);
+            ->json(['message' => 'Unauthorized'], 401);
       }
 
       $user = User::where('email', $request['email'])->firstOrFail();
@@ -65,9 +65,10 @@ class AuthController extends Controller
                         "email": "einstein@email.com",
                   }
       }
-   **/
-   public function user_details($email){
-      $user = User::where('email',$email)->first();
+    **/
+   public function user_details($email)
+   {
+      $user = User::where('email', $email)->first();
       return response()->json([
          "success" => true,
          "message" => "User Details",
@@ -80,130 +81,127 @@ class AuthController extends Controller
     * Logout and delete token
     *
     *
-   **/
-  public function logout(Request $request)
-  {
-     $request->user()->currentAccessToken()->delete();
+    **/
+   public function logout(Request $request)
+   {
+      $request->user()->currentAccessToken()->delete();
 
-     return [
-        'message' => 'You have successfully logged out'
-     ];
-  }
-
-
-    /**
-     * send otp
-     *
-     * @return response()
-     */
-
-public function sendOTP($number) {
+      return [
+         'message' => 'You have successfully logged out'
+      ];
+   }
 
 
-   $user = FacadesDB::table('users')->where('phone_number',$number)->get();
+   /**
+    * send otp
+    *
+    * @return response()
+    */
 
-   if ($user) {
-   try {
+   public function sendOTP($number)
+   {
 
-       $code = rand(100000, 999999);
 
-       UserCode::updateOrCreate([
-           'user_id' => $user[0]->id,
-           'code' => $code
-       ]);
+      $user = FacadesDB::table('users')->where('phone_number', $number)->get();
 
-       $curl = curl_init();
+      if ($user) {
+         try {
 
-       curl_setopt_array($curl, array(
-         CURLOPT_URL => 'https://prsp.jambopay.co.ke/api/api/org/disburseSingleSms/',
-         CURLOPT_RETURNTRANSFER => true,
-         CURLOPT_ENCODING => '',
-         CURLOPT_MAXREDIRS => 10,
-         CURLOPT_TIMEOUT => 0,
-         CURLOPT_FOLLOWLOCATION => true,
-         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-         CURLOPT_CUSTOMREQUEST => 'POST',
-         CURLOPT_POSTFIELDS =>'{
-             "number" : "'.$number.'",
-             "sms" : '.$code.',
+            $code = rand(100000, 999999);
+
+            UserCode::updateOrCreate([
+               'user_id' => $user[0]->id,
+               'code' => $code
+            ]);
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+               CURLOPT_URL => 'https://prsp.jambopay.co.ke/api/api/org/disburseSingleSms/',
+               CURLOPT_RETURNTRANSFER => true,
+               CURLOPT_ENCODING => '',
+               CURLOPT_MAXREDIRS => 10,
+               CURLOPT_TIMEOUT => 0,
+               CURLOPT_FOLLOWLOCATION => true,
+               CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+               CURLOPT_CUSTOMREQUEST => 'POST',
+               CURLOPT_POSTFIELDS => '{
+             "number" : "' . $number . '",
+             "sms" : ' . $code . ',
              "callBack" : "https://....",
              "senderName" : "PASANDA"
        }
        ',
-         CURLOPT_HTTPHEADER => array(
-           'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjozNywibmFtZSI6IkRldmVpbnQgTHRkIiwiZW1haWwiOiJpbmZvQGRldmVpbnQuY29tIiwibG9jYXRpb24iOiIyMyBPbGVuZ3VydW9uZSBBdmVudWUsIExhdmluZ3RvbiIsInBob25lIjoiMjU0NzQ4NDI0NzU3IiwiY291bnRyeSI6IktlbnlhIiwiY2l0eSI6Ik5haXJvYmkiLCJhZGRyZXNzIjoiMjMgT2xlbmd1cnVvbmUgQXZlbnVlIiwiaXNfdmVyaWZpZWQiOmZhbHNlLCJpc19hY3RpdmUiOmZhbHNlLCJjcmVhdGVkQXQiOiIyMDIxLTExLTIzVDEyOjQ5OjU2LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIxLTExLTIzVDEyOjQ5OjU2LjAwMFoifSwiaWF0IjoxNjQ5MzEwNzcxfQ.4y5XYFbC5la28h0HfU6FYFP5a_6s0KFIf3nhr3CFT2I',
-           'Content-Type: application/json'
-         ),
-       ));
+               CURLOPT_HTTPHEADER => array(
+                  'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjozNywibmFtZSI6IkRldmVpbnQgTHRkIiwiZW1haWwiOiJpbmZvQGRldmVpbnQuY29tIiwibG9jYXRpb24iOiIyMyBPbGVuZ3VydW9uZSBBdmVudWUsIExhdmluZ3RvbiIsInBob25lIjoiMjU0NzQ4NDI0NzU3IiwiY291bnRyeSI6IktlbnlhIiwiY2l0eSI6Ik5haXJvYmkiLCJhZGRyZXNzIjoiMjMgT2xlbmd1cnVvbmUgQXZlbnVlIiwiaXNfdmVyaWZpZWQiOmZhbHNlLCJpc19hY3RpdmUiOmZhbHNlLCJjcmVhdGVkQXQiOiIyMDIxLTExLTIzVDEyOjQ5OjU2LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIxLTExLTIzVDEyOjQ5OjU2LjAwMFoifSwiaWF0IjoxNjQ5MzEwNzcxfQ.4y5XYFbC5la28h0HfU6FYFP5a_6s0KFIf3nhr3CFT2I',
+                  'Content-Type: application/json'
+               ),
+            ));
 
-       $response = curl_exec($curl);
+            $response = curl_exec($curl);
 
-       curl_close($curl);
+            curl_close($curl);
 
-       return response()->json(['data' => $user, 'otp' => $code]);
-   } catch (ExceptionHandler $e) {
-       return response()->json(['message' => 'Error occured while trying to send OTP code']);
+            return response()->json(['data' => $user, 'otp' => $code]);
+         } catch (ExceptionHandler $e) {
+            return response()->json(['message' => 'Error occured while trying to send OTP code']);
+         }
+      } else {
+         return response()->json(['message' => 'User is not registered!']);
+      }
    }
 
-   } else {
-       return response()->json(['message' => 'User is not registered!']);
+   /**
+    * verify otp
+    *
+    * @return response()
+    */
+   public function verifyOTP($number, $otp)
+   {
+
+
+      // $phone = $request->only('phone');
+      // $phone = substr($validated['phone'], 1);
+      // $phone = '+254'.$phone;
+      // $phone = str_replace(' ', '', $phone);
+
+      $user = DB::table('users')->where('phone_number', $number)->get();
+
+      // return $user;
+
+      $exists = UserCode::where('user_id', $user[0]->id)
+         ->where('code', $otp)
+         ->where('updated_at', '>=', now()->subMinutes(5))
+         ->latest('updated_at')
+         ->exists();
+
+      if ($exists) {
+         // DB::table('users')
+         // ->where('id', $user->id)
+         // ->update(['status' => 'activated']);
+         //  Log::info('Valid OTP entered');
+         return response()->json(['message' => 'Valid OTP entered']);
+      }
+      // Log::error('Invalid OTP entered');
+      return response()->json(['message' => 'Invalid OTP entered']);
    }
-
-
-}
-
-        /**
-     * verify otp
-     *
-     * @return response()
-     */
-    public function verifyOTP($number, $otp)
-        {
-
-
-            // $phone = $request->only('phone');
-            // $phone = substr($validated['phone'], 1);
-            // $phone = '+254'.$phone;
-            // $phone = str_replace(' ', '', $phone);
-
-            $user = DB::table('users')->where('phone_number', $number)->get();
-
-            // return $user;
-
-            $exists = UserCode::where('user_id', $user[0]->id)
-                    ->where('code', $otp)
-                    ->where('updated_at', '>=', now()->subMinutes(5))
-                    ->latest('updated_at')
-                    ->exists();
-
-            if ($exists) {
-                // DB::table('users')
-                // ->where('id', $user->id)
-                // ->update(['status' => 'activated']);
-               //  Log::info('Valid OTP entered');
-                return response()->json(['message' => 'Valid OTP entered']);
-            }
-            // Log::error('Invalid OTP entered');
-            return response()->json(['message' => 'Invalid OTP entered']);
-
-        }
 
    public function updatePassword(Request $request)
-            {
+   {
 
-            $request->validate([
-                  'phone_number' => 'required|string|exists:users',
-                  'password' => 'required|string|min:6|confirmed',
-                  'password_confirmation' => 'required',
+      $request->validate([
+         'phone_number' => 'required|string|exists:users',
+         'password' => 'required|string|min:6|confirmed',
+         'password_confirmation' => 'required',
 
-            ]);
+      ]);
 
-               $user = User::where('phone_number', $request->phone_number)
-                           ->update(['password' => Hash::make($request->password)]);
+      $user = User::where('phone_number', $request->phone_number)
+         ->update(['password' => Hash::make($request->password)]);
 
 
-               return response()->json(['message' => 'Password has been changed sucessfully']);
-               // DB::table('password_resets')->where(['email'=> $request->email])->delete();
+      return response()->json(['message' => 'Password has been changed sucessfully']);
+      // DB::table('password_resets')->where(['email'=> $request->email])->delete();
 
-            }
+   }
 }
