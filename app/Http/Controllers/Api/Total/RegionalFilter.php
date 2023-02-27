27d8d5b084be51ba1a7456051ce04ebe7813d17f
ngetichishmael <ngetichishmael786@gmail.com>
2023-02-27 23:20:37 +0300

    updates to regional data

--- app/Http/Controllers/Api/Total/RegionalFilter.php
+++ app/Http/Controllers/Api/Total/RegionalFilter.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\total;
 use App\Models\Region;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Area;
 use App\Models\customers;
+use App\Models\Subarea;
 use App\Models\Subregion;
 use App\Models\zone;
 
@@ -28,14 +30,20 @@ class RegionalFilter extends Controller
       $data = array();
       $regions = Region::where('primary_key',  $route_code)->first();
       $subregion = Subregion::where('primary_key',  $route_code)->first();
+      $area = Area::where('primary_key',  $route_code)->first();
+      $subarea = Subarea::where('primary_key',  $route_code)->first();
       $zone = zone::where('primary_key',  $route_code)->first();
 
       if ($regions) {
          $data = customers::where('region_id', $regions->id)->get();
       } else if ($subregion) {
          $data = customers::where('region_id', $subregion->Region->id)->get();
+      } else if ($area) {
+         $data = customers::where('region_id', $area->Subregion->Region->id)->get();
+      } else if ($subarea) {
+         $data = customers::where('region_id', $subarea->Area->Subregion->Region->id)->get();
       } else if ($zone) {
-         $data = customers::where('region_id', $zone->Subregion->Region->id)->get();
+         $data = customers::where('region_id', $zone->subarea->Area->Subregion->Region->id)->get();
       }
       return response()->json(
          [
