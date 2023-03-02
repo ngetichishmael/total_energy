c1ff6302d183c513c0de88eefdea14d0d60679f1
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-02 13:20:45 +0300

    updates to routes

--- app/Http/Controllers/Api/Total/TerritoryController.php
+++ app/Http/Controllers/Api/Total/TerritoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Total;
 
 use App\Http\Controllers\Controller;
+use App\Models\Region;
 use App\Models\Relationship;
 use Illuminate\Http\Request;
 
@@ -10,20 +11,7 @@ class TerritoryController extends Controller
 {
    public function terriory(Request $request)
    {
-      // $route_code = $request->user()->route_code;
-      // $data = array();
-      // $regions = Region::where('primary_key',  $route_code)->first();
-      // $subregion = Subregion::where('primary_key',  $route_code)->first();
-      // $zone = zone::where('primary_key',  $route_code)->first();
-
-      // if ($regions) {
-      //    $data = Region::with('Subregion.Zones')->whereId($regions->id)->get();
-      // } else if ($subregion) {
-      //    $data = Region::with('Subregion.Zones')->whereId($subregion->Region->id)->get();
-      // } else if ($zone) {
-      //    $data = Region::with('Subregion.Zones')->whereId($zone->Subregion->Region->id)->get();
-      // }
-      $data = Relationship::where('has_children', '0')->get();
+      $data = Region::with('Subregion.Area')->get();
       return response()->json(
          [
             'status' => 200,
@@ -36,7 +24,8 @@ class TerritoryController extends Controller
 
    public function routes()
    {
-      $data = Relationship::where('has_children', '0')->get();
+      // $data = Relationship::where('has_children', '0')->get();
+      $data = Region::with('Subregion.Area')->get();
       return response()->json(
          [
             'status' => 200,
