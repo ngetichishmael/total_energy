f6a7855e95e469fc6ffe9227fc9a1918af09e3c6
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-03 21:07:24 +0300

    updates to users

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -30,6 +30,7 @@
                             <th>Phone</th>
                             <th>Account Type</th>
                             <th>Status</th>
+                            <th width="12%">Edit</th>
                             <th width="12%">Actions</th>
                         </tr>
                     </thead>
@@ -46,19 +47,19 @@
                                 <td>{!! $user->account_type !!}</td>
                                 <td>{!! $user->status !!}</td>
                                 <td>
-                                    <div class="d-flex" style="gap:2px;">
-                                        <a href="{{ route('user.edit', $user->user_code) }}"
-                                            class="btn btn-primary btn-sm">Edit</a>
-                                        @if ($$user->status === 'Active')
-                                            <button wire:click.prevent="deactivate({{ $user->id }})"
-                                                onclick="confirm('Are you sure you want to DEACTIVATE this customer?')||event.stopImmediatePropagation()"
-                                                type="button" class="btn btn-success btn-sm">Activate</button>
-                                        @else
-                                            <button wire:click.prevent="activate({{ $user->id }})"
-                                                onclick="confirm('Are you sure you want to ACTIVATE this customer?')||event.stopImmediatePropagation()"
-                                                type="button" class="btn btn-danger btn-sm">Suspend</button>
-                                        @endif
-                                        <a href="{!! route('user.destroy', $user->id) !!}" class="btn btn-danger btn-sm">Delete</a>
+                                    <a href="{{ route('user.edit', $user->user_code) }}"
+                                        class="btn btn-primary btn-sm">Edit</a>
+                                </td>
+                                <td>
+                                    @if ($$user->status === 'Active')
+                                        <button wire:click.prevent="deactivate({{ $user->id }})"
+                                            onclick="confirm('Are you sure you want to DEACTIVATE this customer?')||event.stopImmediatePropagation()"
+                                            type="button" class="btn btn-success btn-sm">Activate</button>
+                                    @else
+                                        <button wire:click.prevent="activate({{ $user->id }})"
+                                            onclick="confirm('Are you sure you want to ACTIVATE this customer?')||event.stopImmediatePropagation()"
+                                            type="button" class="btn btn-danger btn-sm">Suspend</button>
+                                    @endif
                                 </td>
                             </tr>
                         @endforeach
