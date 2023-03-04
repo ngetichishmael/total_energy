b4517b01b219abf4b5b7686cae490ce3ffbe7dd0
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-04 10:51:43 +0300

    updates to vansales

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -12,21 +12,21 @@ use App\Models\Orders;
 class PaymentController extends Controller
 {
 
-    public function index(Request $request)
-    {
+   public function index(Request $request)
+   {
 
-      $user_code= $request->user()->user_code;
-        $orderID = $request->get('orderID');
-        $checking_code = DB::table('orders')->where('order_code', $orderID)->first();
-       // $code =$checking_code->checkin_code;
-        $amount = $request->get('amount');
-        $transactionID = $request->get('transactionID');
-        $paymentMethod = $request->get('paymentMethod');
-        $balance = $checking_code->balance - $amount;
-        //$random = Str::random(20);
-        $ID = $request->user()->id;
+      $user_code = $request->user()->user_code;
+      $orderID = $request->get('orderID');
+      $checking_code = DB::table('orders')->where('order_code', $orderID)->first();
+      // $code =$checking_code->checkin_code;
+      $amount = $request->get('amount');
+      $transactionID = $request->get('transactionID');
+      $paymentMethod = $request->get('paymentMethod');
+      $balance = $checking_code->balance - $amount;
+      //$random = Str::random(20);
+      $ID = $request->user()->id;
 
-   DB::insert('INSERT INTO `order_payments`(
+      DB::insert('INSERT INTO `order_payments`(
                     `amount`,
                     `balance`,
                     `payment_date`,
@@ -37,34 +37,35 @@ class PaymentController extends Controller
                     `created_at`,
                     `updated_at`
                 )
-                VALUES(?,?,?,?,?,?,?,?,? )',[$amount,
-                                $balance,
-                                now(),
-                                $paymentMethod,
-                                $transactionID,
-                                $orderID,
-                                $ID,
-                                now(),
-                                now()]);
+                VALUES(?,?,?,?,?,?,?,?,? )', [
+         $amount,
+         $balance,
+         now(),
+         $paymentMethod,
+         $transactionID,
+         $orderID,
+         $ID,
+         now(),
+         now()
+      ]);
 
-  (string) $payment_status = $balance ==0 ? "PAID": "PARTIAL PAID"; ;
+      (string) $payment_status = $balance == 0 ? "PAID" : "PARTIAL PAID";;
 
-  Orders::where('order_code', '=', $orderID)
-    ->update([
-            'balance'=>$balance,
-            'order_status'=>'DELIVERED',
-            'payment_status'=> $payment_status,
-            'updated_at'=>now()
-    ]);
-    DB::table('sales_targets')
-    ->where('user_code',$user_code)
-    ->increment('AchievedSalesTarget',$amount);
+      Orders::where('order_code', '=', $orderID)
+         ->update([
+            'balance' => $balance,
+            'payment_status' => $payment_status,
+            'updated_at' => now()
+         ]);
+      DB::table('sales_targets')
+         ->where('user_code', $user_code)
+         ->increment('AchievedSalesTarget', $amount);
 
-        return response()->json([
-            "success" => true,
-            "message" => "Successfully",
-            "Result" => $orderID
+      return response()->json([
+         "success" => true,
+         "message" => "Successfully",
+         "Result" => $orderID
 
-        ]);
-    }
+      ]);
+   }
 }
