0516fde129b8d40f721603fc08bea99a62558297
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-07 19:11:06 +0300

    updates total's survey

--- app/Models/SurveyResponses.php
+++ app/Models/SurveyResponses.php
@@ -4,19 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SurveyResponses extends Model
 {
-    use HasFactory;
-    protected $table = 'survey_responses';
+   use HasFactory;
+   protected $table = 'survey_responses';
 
-    protected $fillable = [
-        'survey_code',
-        'question_code',
-        'customer_id',
-        'answer',
-        'reason',
-    
-    ];
+   protected $guarded = [""];
 
+   /**
+    * Get the Customer that owns the SurveyResponses
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+   public function Customer(): BelongsTo
+   {
+      return $this->belongsTo(customers::class, 'customer_id', 'id');
+   }
+   /**
+    * Get the Survery that owns the SurveyResponses
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+   public function Survey(): BelongsTo
+   {
+      return $this->belongsTo(survey::class, 'survey_code', 'code');
+   }
+   /**
+    * Get the Question that owns the SurveyResponses
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+   public function Question(): BelongsTo
+   {
+      return $this->belongsTo(questions::class, 'question_code', 'question_code');
+   }
 }
