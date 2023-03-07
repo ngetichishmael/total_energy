898c53103908ad89a8f659bfaca21ee75c138cf2
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-07 19:15:14 +0300

    updates total's survey

--- app/Models/SurveyResponses.php
+++ app/Models/SurveyResponses.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\survey\questions;
 use App\Models\survey\survey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
