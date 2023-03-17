6ef1880ee58b135814ddab0cd37b2e0628955b39
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-18 02:34:06 +0300

    updates to delivery controller

--- app/Models/Order_items.php
+++ app/Models/Order_items.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\products\product_price;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
