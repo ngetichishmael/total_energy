6814d7545f59d1e2260e0371979dadb86cda7167
ngetichishmael <ngetichishmael786@gmail.com>
2023-03-18 02:31:41 +0300

    updates to delivery controller

--- app/Models/Order_items.php
+++ app/Models/Order_items.php
@@ -4,19 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order_items extends Model
 {
-    protected $table = 'order_items';
-    protected $guarded =[];
+   protected $table = 'order_items';
+   protected $guarded = [];
 
-    /**
-     * Get the Order that owns the Order_items
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function Order(): BelongsTo
-    {
-        return $this->belongsTo(Orders::class, 'order_code', 'order_code');
-    }
+   /**
+    * Get the Order that owns the Order_items
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+   public function Order(): BelongsTo
+   {
+      return $this->belongsTo(Orders::class, 'order_code', 'order_code');
+   }
+   /**
+    * Get the ProductPrice associated with the product_information
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasOne
+    */
+   public function ProductPrice(): HasOne
+   {
+      return $this->hasOne(product_price::class, 'productID', 'productID');
+   }
 }
