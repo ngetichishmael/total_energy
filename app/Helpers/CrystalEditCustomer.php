d61f3e1a690a00fdcef0cc7a5bdbe861a689a94e
ngetichishmael <ngetichishmael786@gmail.com>
2023-05-24 18:34:28 +0300

    updates to customer editting for crystal

--- app/Helpers/CrystalEditCustomer.php
+++ app/Helpers/CrystalEditCustomer.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Validator;
 class CrystalEditCustomer
 {
 
-   public static function  addCustomer($request)
+   public static function  EditCustomer($request)
    {
       $validator = Self::validate($request);
       if ($validator->fails()) {
@@ -24,123 +24,92 @@ class CrystalEditCustomer
                "errors" => $validator->errors()
             ];
       }
-      $image_path = $request->file('image')->store('image', 'public');
-      $customer = Customer::updateOrCreate(
-         [
-            'customer_name' => $request->customer_name,
-            'contact_person' => $request->contact_person,
-         ],
-         [
-            'odoo_uuid' => $request->odoo_uuid,
-            'soko_uuid' => $request->soko_uuid,
-            'company_type' => $request->company_type,
-            'image' => $image_path,
-            'telephone' => $request->phone_number,
-            'phone_number' => $request->phone_number,
-            'mobile' => $request->mobile,
-            'email' => $image_path,
-            'type' => $request->type,
-            'street' => $request->street,
-            'city' => $request->city,
-            'postal_code' => $request->postal_code,
-            'country' => $request->country,
-            'latitude' => $request->latitude,
-            'longitude' => $request->longitude,
-            'manufacturer_number' => $request->manufacturer_number,
-            'route' => $request->route,
-            'route_code' => "Nairobi",
-            'region' => "",
-            'subregion' => "",
-            'zone' => "",
-            'unit' => "",
-            'branch' => $request->branch,
-            'business_code' => $request->business_code,
-         ]
-      );
-      $response = Self::addCustomerToOdoo($request, $customer);
-      if ($response->ok()) {
-         return response()->json([
-            "success" => true,
-            "status" => 200,
-            "message" => "Customer added successfully",
-            "response" => $response->body()
-         ]);
+      $customer = Customer::whereId($request->id)->first();
+      $customerData = [];
+      if (isset($customer)) {
+         $customerData = Customer::create(
+            [
+               "customer_name" => $request->customer_name ?? $customer->customer_name,
+               "account" => $request->account ?? $customer->account,
+               "address" => $request->address ?? $customer->address,
+               "latitude" => $request->latitude ?? $customer->latitude,
+               "longitude" => $request->longitude ?? $customer->longitude,
+               "contact_person" => $request->contact_person ?? $customer->contact_person,
+               "customer_group" => $request->customer_group ?? $customer->customer_group,
+               "price_group" => $request->price_group ?? $customer->price_group,
+               "route" => $request->route ?? $customer->route,
+               "region_id" => $request->route ?? $customer->route,
+               "unit_id" => $request->route ?? $customer->route,
+               "approval" => 'Approved' ?? $customer->approval,
+               "status" => 'Active' ?? $customer->status,
+               "telephone" => $request->telephone ?? $customer->telephone,
+               "manufacturer_number" => $request->manufacturer_number ?? $customer->manufacturer_number,
+               "vat_number" => $request->vat_number ?? $customer->vat_number,
+               "delivery_time" => $request->delivery_time ?? $customer->delivery_time,
+               "city" => $request->city ?? $customer->city,
+               "province" => $request->province ?? $customer->province,
+               "postal_code" => $request->postal_code ?? $customer->postal_code,
+               "country" => $request->country ?? $customer->country,
+               "customer_secondary_group" => $request->customer_secondary_group ?? $customer->customer_secondary_group,
+               "branch" => $request->branch ?? $customer->branch,
+               "email" => $request->email ?? $customer->email,
+               "phone_number" => $request->phone_number ?? $customer->phone_number,
+               "business_code" => $request->user()->business_code ?? $customer->business_code,
+               "created_by" => $request->user()->id ?? $customer->id
+            ]
+         );
       } else {
-         return response()->json([
-            "success" => true,
-            "status" => 401,
-            "message" => "An error occurred while processing",
-            "response" => $response
-         ]);
+         $customerData = Customer::whereId($request->id)->update(
+            [
+               "customer_name" => $request->customer_name ?? $customer->customer_name,
+               "account" => $request->account ?? $customer->account,
+               "address" => $request->address ?? $customer->address,
+               "latitude" => $request->latitude ?? $customer->latitude,
+               "longitude" => $request->longitude ?? $customer->longitude,
+               "contact_person" => $request->contact_person ?? $customer->contact_person,
+               "customer_group" => $request->customer_group ?? $customer->customer_group,
+               "price_group" => $request->price_group ?? $customer->price_group,
+               "route" => $request->route ?? $customer->route,
+               "region_id" => $request->route ?? $customer->route,
+               "unit_id" => $request->route ?? $customer->route,
+               "approval" => 'Approved' ?? $customer->approval,
+               "status" => 'Active' ?? $customer->status,
+               "telephone" => $request->telephone ?? $customer->telephone,
+               "manufacturer_number" => $request->manufacturer_number ?? $customer->manufacturer_number,
+               "vat_number" => $request->vat_number ?? $customer->vat_number,
+               "delivery_time" => $request->delivery_time ?? $customer->delivery_time,
+               "city" => $request->city ?? $customer->city,
+               "province" => $request->province ?? $customer->province,
+               "postal_code" => $request->postal_code ?? $customer->postal_code,
+               "country" => $request->country ?? $customer->country,
+               "customer_secondary_group" => $request->customer_secondary_group ?? $customer->customer_secondary_group,
+               "branch" => $request->branch ?? $customer->branch,
+               "email" => $request->email ?? $customer->email,
+               "phone_number" => $request->phone_number ?? $customer->phone_number,
+               "business_code" => $request->user()->business_code ?? $customer->business_code,
+               "created_by" => $request->user()->id ?? $customer->id
+            ]
+         );
       }
-   }
-   public static function addCustomerToOdoo($request, $customer)
-   {
-      $data = [
-         "id" => $customer->id,
-         "soko_uuid" => Str::uuid(),
-         "company_type" => "",
-         "image" => base64_encode(file_get_contents($request->file('image')->pat‌​h())),
-         "customer_name" => $request->customer_name ?? $customer->customer_name,
-         "telephone" => $request->telephone ?? $customer->telephone,
-         "mobile" => $request->telephone ?? $customer->telephone,
-         "email" => $request->email ?? $customer->email,
-         "type" => "",
-         "contact_person" => $request->contact_person ?? $customer->contact_person,
-         "latitude" => $request->latitude ?? $customer->latitude,
-         "longitude" => $request->longitude ?? $customer->longitude,
-         "address" => [
-            "street" => $request->address ?? $customer->address,
-            "city" => $request->address ?? $customer->address,
-            "postal_code" => $request->address ?? $customer->address,
-            "country" => $request->address ?? $customer->address,
-         ],
-         "metadata" => [
-            "manufacturer_number" => $request->manufacturer_number ?? $customer->manufacturer_number,
-            "route" => $request->route ?? $customer->route,
-            "route_code" => "Nairobi",
-            "region" =>  "Nairobi",
-            "subregion" =>  "",
-            "zone" =>  "",
-            "unit" =>  "",
-            "branch" => $request->branch ?? $customer->branch,
-            "business_code" => $request->user()->business_code ?? $customer->business_code
-         ]
+      return [
+         "success" => true,
+         "status" => 200,
+         "message" => "Customer edit was successfully",
+         "response" => $customerData
       ];
-
-      info("Sending for Odoo Customer");
-      info($data);
-      DB::table('leads_targets')
-         ->where('user_code', $request->user()->user_code)
-         ->increment('AchievedLeadsTarget');
-      $response = Http::withHeaders([
-         "Content-Type" => "application/json",
-      ])->post(env("BASE_URL"), $data);
-      return $response;
    }
    public static function validate($request)
    {
       $validator           =  Validator::make($request->all(), [
-         "customer_name"   => "required|unique:customers",
+         "customer_name"   => "required",
          "contact_person"  => "required",
          "business_code"   => "required",
          "created_by"      => "required",
-         "phone_number"    => "required|unique:customers",
+         "phone_number"    => "required",
          "latitude"        => "required",
          "longitude"       => "required",
          "image" => 'required|image|mimes:jpg,png,jpeg,gif,svg',
       ]);
-
-      if ($validator->fails()) {
-         return response()->json(
-            [
-               "status" => 401,
-               "message" =>
-               "validation_error",
-               "errors" => $validator->errors()
-            ],
-            403
-         );
-      }
+      return $validator;
    }
 }
