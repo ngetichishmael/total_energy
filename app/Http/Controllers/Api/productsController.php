0fece4b74815c0b1a857468d354225fb1b0dbed1
ngetichishmael <ngetichishmael786@gmail.com>
2023-06-21 11:39:12 +0300

    updates to customers products

--- app/Http/Controllers/Api/productsController.php
+++ app/Http/Controllers/Api/productsController.php
@@ -26,23 +26,25 @@ class productsController extends Controller
    {
       $route_code = $request->user()->route_code;
       $region_id = Region::whereId($route_code)->first();
-      $products = product_information::join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
-         ->join('product_price', 'product_price.productID', '=', 'product_information.id')
-         ->where('product_price.branch_id', $region_id->id)
-         ->select(
-            'product_price.branch_id as region',
-            'product_information.id as productID',
-            'product_information.created_at as date',
-            'product_price.selling_price as wholesale_price',
-            'product_price.buying_price as retail_price',
-            'product_information.product_name as product_name',
-            'product_inventory.current_stock as stock',
-            'product_information.created_at as date',
-            'product_information.business_code as business_code',
-            'sku_code',
-            'brand',
-            'category'
-         )
+      $query = product_information::join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
+         ->join('product_price', 'product_price.productID', '=', 'product_information.id');
+      if ($region_id == 12) {
+         $query->where('product_price.branch_id', $region_id->id);
+      }
+      $products = $query->select(
+         'product_price.branch_id as region',
+         'product_information.id as productID',
+         'product_information.created_at as date',
+         'product_price.selling_price as wholesale_price',
+         'product_price.buying_price as retail_price',
+         'product_information.product_name as product_name',
+         'product_inventory.current_stock as stock',
+         'product_information.created_at as date',
+         'product_information.business_code as business_code',
+         'sku_code',
+         'brand',
+         'category'
+      )
          ->get();
 
       return response()->json([
