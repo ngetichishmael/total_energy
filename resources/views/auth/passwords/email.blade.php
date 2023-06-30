630b6ae1b57ab27ab1e27921b47378a8e6ed3cbb
rickxy <omundieric6@gmail.com>
2023-06-30 11:56:36 +0300

    update

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,57 +1,75 @@
+
+
+
 <!DOCTYPE html>
 <html class="loading" lang="en" data-textdirection="ltr">
-   @section('title','Reset Password')
-   <!-- BEGIN: Head-->
-   @include('partials._head')
-   <!-- END: Head-->
-   <!-- BEGIN: Body-->
-   <body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="blank-page">
-      <!-- BEGIN: Content-->
-      <div class="app-content content ">
-         <div class="content-overlay"></div>
-         <div class="header-navbar-shadow"></div>
-         <div class="content-wrapper">
-            <div class="content-header row">
-            </div>
-            <div class="content-body">
-               <div class="auth-wrapper auth-v1 px-2">
-                  <div class="auth-inner py-2">
-                     <!-- Login v1 -->
-                     <div class="card mb-0">
-                        <div class="card-body">
-                           <a href="#" class="brand-logo">
-                              
-                              <h2 class="brand-text text-primary ms-1">Pasanda</h2>
-                           </a>
-                           <h4 class="card-title mb-1">Reset Password</h4>
-                           @if (session('status'))
+@section('title', 'Reset Password')
+<!-- BEGIN: Head-->
+@include('partials._head')
+<!-- END: Head-->
+<!-- BEGIN: Body-->
+
+<body>
+    <div class="dashboard-landing">
+        <div class="left-side">
+            <nav>
+               
+            </nav>
+            <img src="{{ asset('app-assets/images/loginpage.svg') }}" alt="" class="img-fluid">
+            <!-- <h1 class="title">Total Energies</h1> -->
+        </div>
+        <div class="right-side">
+            <div class="login-fields">
+                <!-- Login v1 -->
+                <div>
+                      <div class="card-body">
+                    <div style="display: flex; justify-content: center;">
+                        <img src="{{ asset('app-assets/images/logo.png') }}" class="logo" alt="kenMeat" style="width:250px; height:150px;"/>
+                    </div>
+
+                    <div class="card-body">
+                    <h4 class="mb-1 card-title"> Forgot your Password? 🔒 </h4>
+                        <p class="mb-2 card-text"> Enter Email to associated with your account</p>
+                 
+                        @if (session('status'))
                               <div class="alert alert-success">
                                  {{ session('status') }}
                               </div>
-                           @endif
-                           <form class="auth-login-form mt-2" action="{{ route('password.email') }}" method="POST">
-                              @csrf
-                              <div class="mb-1">
-                                 <label for="login-email" class="form-label">Email</label>
-                                 <input type="email" class="form-control" name="email" placeholder="john@example.com" required/>
-                                 @if($errors->has('email'))
-                                    <span class="help-block">
-                                       <strong class="text-danger">{{ $errors->first('email') }}</strong>
-                                    </span>
-                                 @endif
-                              </div>
-                              <button type="submit" class="btn btn-primary w-100" tabindex="4">Get reset link</button>
-                           </form>
-                        </div>
-                     </div>
-                     <!-- /Login v1 -->
-                  </div>
-               </div>
+                        @endif
+                        @if($errors->has('email'))
+                              <span class="help-block">
+                                 <strong class="text-danger">{{ $errors->first('email') }}</strong>
+                              </span>
+                        @endif
+                 
+                        <form class="mt-2 auth-login-form" action="{{ route('password.email') }}" method="POST">
+                            @csrf
+                            <div class="mb-1">
+                                <label for="login-email" class="form-label">Email</label>
+                                <input type="text" class="form-control" id="login-email" name="email"
+                                    placeholder="john@example.com" aria-describedby="login-email" tabindex="1"
+                                    autofocus />
+                           
+                            </div>
+                      
+                     
+                            <button type="submit" class="btn btn-primary w-100" tabindex="4" style="background: linear-gradient(to right, red, blue); color: white;">Send Reset Link</button>
+
+                        </form>
+                           <p class="text-right mt-2" style="text-align: right;">
+                              <a href="{{ route('logout') }}">
+                                 <i data-feather="chevron-left"></i> Back to login
+                              </a>
+                           </p>
+                    </div>
+                </div>
+                <!-- /Login v1 -->
             </div>
-         </div>
-      </div>
-      <!-- END: Content-->
-      @include('partials._javascripts')
-   </body>
-   <!-- END: Body-->
+        </div>
+    </div>
+    <!-- END: Content-->
+    @include('partials._javascripts')
+</body>
+<!-- END: Body-->
+
 </html>
--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,5 +1,21 @@
 <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
-    <div class="shadow-bottom"></div>
+<div class="navbar-header mb-1">
+        <ul class="nav navbar-nav flex-row">
+            <li class="nav-item me-auto">
+                <a class="" href="#">
+                   
+                    <center>
+                  <img src="{!! asset('app-assets/images/logoo.png') !!}" alt="Soko Flow" class="img" style="height: 60px; width: 220px;">
+               </center>
+                </a>
+            </li>
+        <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pe-0" data-bs-toggle="collapse"><i class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i><i class="d-none d-xl-block collapse-toggle-icon font-medium-4  text-primary" data-feather="disc" data-ticon="disc"></i></a></li> 
+        </ul>
+    </div>  
+
+
+
+<div class="shadow-bottom"></div>
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
             <li class=" nav-item {!! Nav::isRoute('app.dashboard') !!}">
