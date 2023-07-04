b795964c2152be2ab1188d8bc8331bd61be82545
rickxy <omundieric6@gmail.com>
2023-07-04 16:17:42 +0300

    update

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -2,11 +2,9 @@
 <div class="navbar-header mb-1">
         <ul class="nav navbar-nav flex-row">
             <li class="nav-item me-auto">
-                <a class="" href="#">
-
-                    <center>
-                  <img src="{!! asset('app-assets/images/logoo.png') !!}" alt="Soko Flow" class="img" style="height: 60px; width: 220px;">
-               </center>
+            <a class="navbar-brand" href="{{ url('/dashboard') }}">
+                        
+                    <h2 class="brand-text" style="color:#FFFFFF; font-size:28px;  font-family: Montserrat;">Total Energies</h2>
                 </a>
             </li>
         <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pe-0" data-bs-toggle="collapse"><i class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i><i class="d-none d-xl-block collapse-toggle-icon font-medium-4  text-primary" data-feather="disc" data-ticon="disc"></i></a></li>
