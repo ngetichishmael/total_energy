c7c70fcd49ace878552809652932e9e1419cdbd9
ngetichishmael <ngetichishmael786@gmail.com>
2023-07-14 11:52:33 +0300

    Updates to customer group

--- app/Http/Livewire/Customers/Dashboard.php
+++ app/Http/Livewire/Customers/Dashboard.php
@@ -14,71 +14,70 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class Dashboard extends Component
 {
-   use WithPagination;
-   protected $paginationTheme = 'bootstrap';
-   public $perPage = 10;
-   public ?string $search = null;
-   public ?string $start = null;
-   public ?string $end = null;
-   public function render()
-   {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    public $perPage = 10;
+    public $search = null;
+    public $start = null;
+    public ?string $end = null;
+    public function render()
+    {
 
-      return view('livewire.customers.dashboard', [
-         'contacts' => $this->getCustomer()
-      ]);
-   }
-   public function export()
-   {
-      return Excel::download(new ExportsCustomers, 'customers.xlsx');
-   }
-   public function deactivate($id)
-   {
-      customers::whereId($id)->update(
-         ['approval' => "Suspended"]
-      );
-      return redirect()->to('/customer');
-   }
-   public function activate($id)
-   {
-      customers::whereId($id)->update(
-         ['approval' => "Approved"]
-      );
+        return view('livewire.customers.dashboard', [
+            'contacts' => $this->getCustomer(),
+        ]);
+    }
+    public function export()
+    {
+        return Excel::download(new ExportsCustomers, 'customers.xlsx');
+    }
+    public function deactivate($id)
+    {
+        customers::whereId($id)->update(
+            ['approval' => "Suspended"]
+        );
+        return redirect()->to('/customer');
+    }
+    public function activate($id)
+    {
+        customers::whereId($id)->update(
+            ['approval' => "Approved"]
+        );
 
-      return redirect()->to('/customer');
-   }
-   public function areas()
-   {
-      $user = Auth::user();
-      $subregions = Subregion::where('region_id', $user->route_code)->pluck('id');
-      $areas = Area::whereIn('subregion_id', $subregions)->pluck('id');
-      return $areas;
-   }
-   public function getCustomer()
-   {
-      $searchTerm = '%' . $this->search . '%';
-      $query = customers::search($searchTerm)
-         ->orderBy('id', 'DESC');
-      if (Auth::user()->account_type != 'Admin') {
-         $query->whereIn('unit_id', $this->areas());
-      }
-      if (is_null($this->start) && is_null($this->end)) {
-         $contacts = $query->paginate($this->perPage);
-         return $contacts;
-      }
-      if (!is_null($this->start)) {
-         $end = Carbon::now()->endOfMonth()->format('Y-m-d');
-         $query->whereBetween('created_at', [$this->start, $end]);
-      }
+        return redirect()->to('/customer');
+    }
+    public function areas()
+    {
+        $user = Auth::user();
+        $subregions = Subregion::where('region_id', $user->route_code)->pluck('id');
+        $areas = Area::whereIn('subregion_id', $subregions)->pluck('id');
+        return $areas;
+    }
+    public function getCustomer()
+    {
+        $searchTerm = '%' . $this->search . '%';
+        $query = customers::search($searchTerm)
+            ->orderBy('id', 'DESC');
+        if (Auth::user()->account_type != 'Admin') {
+            $query->whereIn('unit_id', $this->areas());
+        }
+        if (is_null($this->start) && is_null($this->end)) {
+            $contacts = $query->paginate($this->perPage);
+            return $contacts;
+        }
+        if (!is_null($this->start)) {
+            $end = Carbon::now()->endOfMonth()->format('Y-m-d');
+            $query->whereBetween('created_at', [$this->start, $end]);
+        }
 
-      if (!is_null($this->start) && Carbon::parse($this->start)->isSameDay(Carbon::parse($this->end))) {
-         $query->where('created_at', 'LIKE', '%'.$this->start. '%');
-      }
+        if (!is_null($this->start) && Carbon::parse($this->start)->isSameDay(Carbon::parse($this->end))) {
+            $query->where('created_at', 'LIKE', '%' . $this->start . '%');
+        }
 
-
-      $contacts = $query->paginate($this->perPage);
-      return $contacts;
-   }
-   public function updatedStart()
+        $contacts = $query->paginate($this->perPage);
+        return $contacts;
+    }
+    public function updatedStart()
     {
         $this->mount();
         $this->render();
