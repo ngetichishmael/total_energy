7a5b2e6dab94fa01ffbc4ea70291a25aebf5073b
rickxy <omundieric6@gmail.com>
2023-07-14 14:34:01 +0300

    update

--- resources/views/app/users/create.blade.php
+++ resources/views/app/users/create.blade.php
@@ -73,7 +73,7 @@
                                                         name="account_type" required>
                                                         <option value="">Select Category</option>
                                                         <option value="Admin">Administator</option>
-                                                        <!-- <option value="Manager">Manager</option> -->
+                                                        <option value="Distributors">Distributors</option>
                                                         <option value="Sales">Sales Agent</option>
                                                         <option value="Lube Sales Executive">Lube Sales Executive</option>
                                                         <option value="Lube Merchandizers">Lube Merchandizers</option>
