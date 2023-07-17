175990c575cbe70b903b2abdc4eb761eda31d43a
ngetichishmael <ngetichishmael786@gmail.com>
2023-07-17 21:25:06 +0300

    Updates to customers mapping

--- app/Http/Controllers/Api/CurrentDeviceInformationController.php
+++ app/Http/Controllers/Api/CurrentDeviceInformationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\CurrentDeviceInformation;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CurrentDeviceInformationController extends Controller
@@ -11,21 +12,49 @@ class CurrentDeviceInformationController extends Controller
     public function postCurrentDeviceInformation(Request $request)
     {
 
-      CurrentDeviceInformation::create(
-         [
-            "user_code"=>$request->user()->user_code,
-            "current_gps"=>$request->current_gps,
-            "current_battery_percentage"=>$request->current_battery_percentage,
-            "device_code"=>$request->device_code,
-            "IMEI"=>$request->IMEI,
-            "fcm_token"=>$request->fcm_token,
-            "android_version"=>$request->android_version
-         ]
-      );
+        CurrentDeviceInformation::create(
+            [
+                "user_code" => $request->user()->user_code,
+                "current_gps" => $request->current_gps,
+                "current_battery_percentage" => $request->current_battery_percentage,
+                "device_code" => $request->device_code,
+                "IMEI" => $request->IMEI,
+                "fcm_token" => $request->fcm_token,
+                "android_version" => $request->android_version,
+            ]
+        );
 
-      return response()->json([
-         "success" => true,
-         "message" => "Data posted successfully",
-      ]);
+        return response()->json([
+            "success" => true,
+            "message" => "Data posted successfully",
+        ]);
+    }
+    public function getUserCoordinates(Request $request, $userCode)
+    {
+        // Fetch the marker data for the specified userCode
+        $markerData = CurrentDeviceInformation::where('user_code', $userCode)->orderBy('id', 'DESC')->limit(25)->get();
+
+        // Prepare the data for the map
+        $markers = [];
+        foreach ($markerData as $data) {
+            $coordinates = explode(',', $data->current_gps);
+            $lat = (float) $coordinates[0];
+            $lng = (float) $coordinates[1];
+
+            // Add the marker data to the $markers array
+            $markers[] = [
+                'lat' => $lat,
+                'lng' => $lng,
+                'title' => User::where('user_code', $data->user_code)->pluck('name')->implode(''),
+                'battery' => $data->current_battery_percentage,
+                'android_version' => $data->android_version,
+                'IMEI' => $data->IMEI,
+                'description' => $data->updated_at->diffForHumans(),
+                // Add other marker properties as needed
+            ];
+        }
+
+        // Return the marker data as JSON response
+        return response()->json($markers);
     }
 }
--- app/Http/Livewire/Maps/Agents/Dashboard.php
+++ app/Http/Livewire/Maps/Agents/Dashboard.php
@@ -1,5 +1,5 @@
 <?php
-
+// app/Http/Livewire/Maps/Agents/Dashboard.php
 namespace App\Http\Livewire\Maps\Agents;
 
 use App\Models\CurrentDeviceInformation;
@@ -8,35 +8,65 @@ use Livewire\Component;
 
 class Dashboard extends Component
 {
-   public string $typeMap = 'roadmap';
-   public function render()
-   {
-      $information = CurrentDeviceInformation::orderBy('id', 'ASC')->get();
-      $data = $information->groupBy('user_code');
-      foreach ($data as $value) {
-         // dd($value);
-         $initialMarkers = [];
-         foreach ($value as $info) {
-            $myArray = explode(',', $info['current_gps']);
-            $array = [
-               'title' => User::where('user_code', $info->user_code)->pluck('name')->implode(''),
-               'user_code' => $info->user_code,
-               'lat' => $myArray[0],
-               'lng' => $myArray[1],
-               'position' => [
-                  'lat' => (float)$myArray[0],
-                  'lng' => (float)$myArray[1],
-               ],
-               'battery' => $info->current_battery_percentage,
-               'android_version' => $info->android_version,
-               'IMEI' => $info->IMEI,
-               'description' => $info->updated_at->diffForHumans(),
+    public string $typeMap = 'roadmap';
+    public bool $customerListVisible = true;
+    public array $mapMarkers = [];
+    public function render()
+    {
+        $initialMarkers = [];
+        $information = CurrentDeviceInformation::orderBy('id', 'ASC')->get();
+        $data = $information->groupBy('user_code');
+        $markersByTitle = [];
+
+        foreach ($data as $value) {
+            foreach ($value as $info) {
+                $myArray = explode(',', $info['current_gps']);
+                $array = [
+                    'title' => User::where('user_code', $info->user_code)->pluck('name')->implode(''),
+                    'user_code' => $info->user_code,
+                    'lat' => $myArray[0],
+                    'lng' => $myArray[1],
+                    'position' => [
+                        'lat' => (float) $myArray[0],
+                        'lng' => (float) $myArray[1],
+                    ],
+                    'battery' => $info->current_battery_percentage,
+                    'android_version' => $info->android_version,
+                    'IMEI' => $info->IMEI,
+                    'description' => $info->updated_at->diffForHumans(),
+                ];
+                array_push($initialMarkers, $array);
+
+                // Use array_key_exists() to check if the title already exists in $markersByTitle
+                if (!array_key_exists($array['title'], $markersByTitle)) {
+                    $markersByTitle[$array['title']][] = $array;
+                }
+                // $markersByTitle[$array['title']] = [];
+            }
+        }
+
+        return view('livewire.maps.agents.dashboard', [
+            'initialMarkers' => $initialMarkers,
+            'markersByTitle' => $markersByTitle,
+        ]);
+    }
+    public function toggleCustomerList()
+    {
+        $this->customerListVisible = !$this->customerListVisible;
+    }
+
+    public function plotMarkers($userCode)
+    {
+        $information = CurrentDeviceInformation::where('user_code', $userCode)->get();
+        $mapMarkers = $information->map(function ($info) {
+            $myArray = explode(',', $info->current_gps);
+            return [
+                'lat' => (float) $myArray[0],
+                'lng' => (float) $myArray[1],
             ];
-            array_push($initialMarkers, $array);
-         }
-      }
-      return view('livewire.maps.agents.dashboard', [
-         'initialMarkers' => $initialMarkers
-      ]);
-   }
+        })->toArray();
+
+        // Emit an event with the plotted markers data to update the map
+        $this->emit('updateMapMarkers', $mapMarkers);
+    }
 }
