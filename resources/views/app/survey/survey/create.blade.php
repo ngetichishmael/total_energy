43911011881493149af744a47113dc433091e72e
ngetichishmael <ngetichishmael786@gmail.com>
2023-07-24 14:51:20 +0300

    Updates to survey

--- resources/views/app/survey/survey/create.blade.php
+++ resources/views/app/survey/survey/create.blade.php
@@ -1,103 +1,92 @@
 @extends('layouts.app')
-@section('title','Add Survey')
+@section('title', 'Add Survey')
 
 @section('content')
-   <div class="content-header row">
-      <div class="content-header-left col-md-12 col-12 mb-2">
-         <div class="row breadcrumbs-top">
-            <div class="col-12">
-               <h2 class="content-header-title float-start mb-0">Survey </h2>
-               <div class="breadcrumb-wrapper">
-                  <ol class="breadcrumb">
-                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                     <li class="breadcrumb-item"><a href="#">Survey</a></li>
-                     <li class="breadcrumb-item active">Create</li>
-                  </ol>
-               </div>
+    <div class="content-header row">
+        <div class="content-header-left col-md-12 col-12 mb-2">
+            <div class="row breadcrumbs-top">
+                <div class="col-12">
+                    <h2 class="content-header-title float-start mb-0">Survey </h2>
+                    <div class="breadcrumb-wrapper">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Survey</a></li>
+                            <li class="breadcrumb-item active">Create</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-   @include('partials._messages')
-   <div class="card">
-      <div class="card-body">
-         {!! Form::open(array('route' => 'survey.store','enctype'=>'multipart/form-data')) !!}
-         @csrf
-            <div class="row">
-               <div class="col-md-6">
-                  <div class="form-group mb-1">
-                     {!! Form::label('title', 'Title', array('class'=>'control-label')) !!}
-                     {!! Form::text('title', null, array('class' => 'form-control', 'placeholder' => 'Title', 'required')) !!}
-                  </div>
-                  {{-- <div class="form-group mb-1">
-                     {!! Form::label('title','Category', array('class'=>'control-label')) !!}
-                     {{ Form::select('category',$category, null, ['class' => 'form-control', 'required' => '']) }}
-                  </div> --}}
-                  {{-- <div class="form-group mb-1">
-                     {!! Form::label('title','Difficulty', array('class'=>'control-label')) !!}
-                     {{ Form::select('difficulty',['easy'=>'Easy','medium'=>'Medium','difficult'=>'Difficult'], null, ['class' => 'form-control']) }}
-                  </div> --}}
-                  <div class="form-group mb-1">
-                     {!! Form::label('type','Type', array('class'=>'control-label')) !!}
-                     {{ Form::select('type',['online'=>'Online'], null, ['class' => 'form-control']), 'required' }}
-                  </div>
-               </div>
-               <div class="col-md-6">
-                  <div class="form-group mb-1">
-                     {!! Form::label('type','Visibility', array('class'=>'control-label')) !!}
-                     {{ Form::select('visibility',['Public'=>'Public','Private'=>'Private'], null, ['class' => 'form-control']) }}
-                  </div>
-                  <div class="form-group mb-1">
-                     {!! Form::label('title','Category Status', array('class'=>'control-label')) !!}
-                     {{ Form::select('status',['Active'=>'Active','Closed'=>'Closed'], null, ['class' => 'form-control', 'required' => '']) }}
-                  </div>
-                  {{-- <div class="form-group mb-1">
-                     <label>Image</label>
-                     {!! Form::file('image',array('class' => 'form-control', 'id' => 'thumbnail', 'files'=> true)) !!}
-                  </div> --}}
-               </div>
-               <div class="col-md-12 mb-1">
-                  <div class="row">
-                     <div class="col-md-6">
-                        <div class="form-group">
-                           <label for="">Start Date</label>
-                           {!! Form::date('start_date',null,['class'=>'form-control'],'required') !!}
+        </div>
+    </div>
+    @include('partials._messages')
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('survey.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group mb-1">
+                            <label for="title">Title</label>
+                            <input type="text" name="title" class="form-control" placeholder="Title" required>
                         </div>
-                     </div>
-                     <div class="col-md-6">
-                        <div class="form-group">
-                           <label for="">End Date</label>
-                           {!! Form::date('end_date',null,['class'=>'form-control']) !!}
+                        <div class="form-group mb-1">
+                            <label for="type">Type</label>
+                            <select name="type" class="form-control" required>
+                                <option value="online">Online</option>
+                            </select>
                         </div>
-                     </div>
-                  </div>
-               </div>
-               {{-- <div class="col-md-12 mb-1">
-                  <div class="panel-body">
-                     <div class="form-group">
-                        <label for="description">Link to sales person</label>
-                        {!! Form::select('sales_person',[$users],null,['class'=>'form-control my-editor']) !!}
-                     </div>
-                  </div>
-               </div> --}}
-               <div class="col-md-12 mb-1">
-                  <div class="panel-body">
-                     <div class="form-group">
-                        <label for="">Description</label>
-                        {!! Form::textarea('description',null,['class'=>'form-control my-editor', 'size' => '6x6', 'placeholder'=>'caption']) !!}
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-12">
-                  <div class="form-group">
-                     <center>
-                        {!! Form::submit('Add Survey',array('class' =>'btn btn-success btn-sm submit')) !!}
-                        <img src="{!! asset('assets/images/loader.gif') !!}" alt="" class="submit-load" style="width: 10%">
-                     </center>
-                  </div>
-               </div>
-            </div>
-         {!! Form::close() !!}
-      </div>
-   </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group mb-1">
+                            <label for="visibility">Visibility</label>
+                            <select name="visibility" class="form-control">
+                                <option value="Public">Public</option>
+                                <option value="Private">Private</option>
+                            </select>
+                        </div>
+                        <div class="form-group mb-1">
+                            <label for="status">Category Status</label>
+                            <select name="status" class="form-control" required>
+                                <option value="Active">Active</option>
+                                <option value="Closed">Closed</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mb-1">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="start_date">Start Date</label>
+                                    <input type="date" name="start_date" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="end_date">End Date</label>
+                                    <input type="date" name="end_date" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mb-1">
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <textarea name="description" class="form-control my-editor" rows="6" placeholder="caption"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <center>
+                                <button type="submit" class="btn btn-success btn-sm submit">Add Survey</button>
+                                <img src="{{ asset('assets/images/loader.gif') }}" alt="" class="submit-load"
+                                    style="width: 10%">
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
