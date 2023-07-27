559f2bbebcf2f75ad9f0b48a2b50d9e73a8f01bd
ngetichishmael <ngetichishmael786@gmail.com>
2023-07-28 00:01:01 +0300

    updates to customer controller

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Api\AppsPermissionController;
 use App\Http\Controllers\Api\CompanyRouteController;
 use App\Http\Controllers\Api\CurrentDeviceInformationController;
-use App\Http\Controllers\api\CustomerDataController;
 use App\Http\Controllers\Api\CustomersProductsController;
 use App\Http\Controllers\Api\CustomerVisitsOrders;
 use App\Http\Controllers\Api\DeliveriesController;
@@ -209,6 +208,6 @@ Route::group(['namespace' => 'Api'], function () {
     Route::get('/get/targets/{type}', [TargetsUIController::class, "getTarget"])->name('getUITargets');
 
     //Customers
-    Route::get('/get/ui/customers', [CustomerDataController::class, "getCustomers"])->name('getUICustomers');
+    Route::get('/get/ui/customers', 'CustomerDataController@getCustomers')->name('getUICustomers');
 
 });
