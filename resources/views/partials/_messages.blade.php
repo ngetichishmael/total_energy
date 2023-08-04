08e9450d572395536ceca8d6c1a44c46788c21eb
rickxy <omundieric6@gmail.com>
2023-08-04 14:43:18 +0300

    update

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -1,11 +1,30 @@
 @if (count($errors) > 0)
-	<div class="alert alert-danger no-border">
-		<button type="button" class="close" data-dismiss="alert"><span>×</span><span class="sr-only">Close</span></button>
-		<span class="text-semibold">Errors!</span><br>
-		<ul>
-			@foreach ($errors->all() as $error)
-				<li>{{ $error }}</li>
+
+
+   <section id="alerts-closable">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="card">
+                   
+                        <div class="card-body">
+                     
+                          <div class="demo-spacing-0">
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                              <div class="alert-body">
+                              <i data-feather="star"></i>
+                       
+							  @foreach ($errors->all() as $error)
+				{{ $error }}
 			@endforeach
-		</ul>
-   </div>
+                              </div>
+                              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
 @endif
