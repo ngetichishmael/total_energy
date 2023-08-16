8e623df2591e6298794fe1c783dd6511759f6316
rickxy <omundieric6@gmail.com>
2023-08-16 09:48:34 +0300

    update

--- resources/views/livewire/user/dashboard.blade.php
+++ resources/views/livewire/user/dashboard.blade.php
@@ -1,6 +1,61 @@
+<div class="card">
+    <h5 class="card-header"></h5>
+    <div class="pt-0 pb-2 d-flex justify-content-between align-items-center mx-50 row">
+        <div class="col-md-3 user_role">
+            <div class="input-group input-group-merge">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i data-feather="search"></i></span>
+                </div>
+                <input wire:model.debounce.300ms="search" type="text" id="fname-icon" class="form-control"
+                    name="fname-icon" placeholder="Search" />
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="form-group">
+                <label for="validationTooltip01">Start Date</label>
+                <input wire:model="start" name="startDate" type="date" class="form-control" id="validationTooltip01"
+                    placeholder="YYYY-MM-DD HH:MM" required />
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="form-group">
+                <label for="validationTooltip01">End Date</label>
+                <input wire:model="end" name="endDate" type="date" class="form-control" id="validationTooltip01"
+                    placeholder="YYYY-MM-DD HH:MM" required />
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="form-group">
+                <label for="">Perpage: </label>
+                <select wire:model="perPage" class="form-control">
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="btn-group">
+                <button type="button" class="btn btn-icon btn-outline-primary dropdown-toggle" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false" wire:loading.attr="disabled">
+                    <img src="{{ asset('assets/img/excel.png') }}" alt="Export Excel" width="15" height="15">
+                    Export
+                </button>
+                <div class="dropdown-menu dropdown-menu-left">
+                    <a class="dropdown-item" wire:click="export" id="exportExcelBtn">Excel</a>
+                    <a class="dropdown-item" wire:click="exportCSV" id="exportCsvBtn">CSV</a>
+                    <a class="dropdown-item" wire:click="exportPDF" id="exportPdfBtn">PDF</a>
+                    <!-- <a class="dropdown-item" wire:click="printAndRedirect" id="printBtn">Print</a> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@include('partials.stickymenu')
+<br>
 <div class="row">
-    @include('partials.stickymenu')
-    <div class="col-md-8">
+    <div class="col-md-12">
         <div class="card card-inverse">
            <div class="card-body">
               <table id="data-table-default" class="table table-striped table-bordered">
--- resources/views/partials/stickymenu.blade.php
+++ resources/views/partials/stickymenu.blade.php
@@ -1,16 +1,16 @@
-<div class="row">
+<div class="row" style="padding-left:6%;">
     <div class="col-md-12" style="padding:10px">
+        <a href="{{ route('clients.reports') }}" class="btn btn-primary" style="font-size: 12px;">Customers</a>
         <a href="{{ route('preorders.reports') }}" class="btn btn-primary" style="font-size: 12px;">Preorders</a>
         <a href="{{ route('vansales.reports') }}" class="btn btn-primary" style="font-size: 12px;">Vansales</a>
         <a href="{{ route('delivery.reports') }}" class="btn btn-primary" style="font-size: 12px;">Delivery</a>
         <!-- <a href="{{ route('supplier.reports') }}" class="btn btn-primary" style="font-size: 12px;">Suppliers</a> -->
         <!-- <a href="{{ route('distributor.reports') }}" class="btn btn-primary" style="font-size: 12px;">Distributors</a> -->
-        <a href="{{ route('warehouse.reports') }}" class="btn btn-primary" style="font-size: 12px;">Warehouse</a>
-        <a href="{{ route('regional.reports') }}" class="btn btn-primary" style="font-size: 12px;">Regional</a>
+        <!-- <a href="{{ route('warehouse.reports') }}" class="btn btn-primary" style="font-size: 12px;">Warehouse</a>
+        <a href="{{ route('regional.reports') }}" class="btn btn-primary" style="font-size: 12px;">Regional</a> -->
         <a href="{{ route('visitation.reports') }}" class="btn btn-primary" style="font-size: 12px;">Visitation</a>
         <a href="{{ route('target.reports') }}" class="btn btn-primary" style="font-size: 12px;">Targets</a>
         <a href="{{ route('sidai.reports') }}" class="btn btn-primary" style="font-size: 12px;">Total Users</a>
-        <a href="{{ route('clients.reports') }}" class="btn btn-primary" style="font-size: 12px;">Customers</a>
         <a href="{{ route('payments.reports') }}" class="btn btn-primary" style="font-size: 12px;">Payments</a>
         <a href="{{ route('inventory.reports') }}" class="btn btn-primary" style="font-size: 12px;">Inventory</a>
     </div>
