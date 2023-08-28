8d7986365b6ca84eab06222ec61f5b01bf572060
rickxy <omundieric6@gmail.com>
2023-08-28 13:25:50 +0300

    update

--- app/Models/Route_sales.php
+++ app/Models/Route_sales.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Route_sales extends Model
 {
    protected $table = 'route_sales';
+
+   public function user(): HasMany
+   {
+      return $this->hasMany(User::class, 'id', 'userID');
+   }
 }
