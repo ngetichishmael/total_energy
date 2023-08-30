c4ce624880ba4fb0abbafc7f5ee9c8a0fb1bb817
rickxy <omundieric6@gmail.com>
2023-08-30 12:55:05 +0300

    update

--- app/Http/Livewire/Target/Index.php
+++ app/Http/Livewire/Target/Index.php
@@ -12,46 +12,36 @@ class Index extends Component
     protected $paginationTheme = 'bootstrap';
     public $start;
     public $end;
-    public $search = '';
-    public $perPage = 15; // Set a default per page value
+    public ?string $search = null;
+    public $perPage = 15; 
     
     use WithPagination;
 
-    public function updatedStart($value)
-   {
-      $this->start = $value;
-   }
-
-   public function updatedEnd($value)
-   {
-      $this->end = $value;
-   }
 
 
-   public function render()
-    {
-        return view('livewire.target.index', [
-            'targets' => $this->data()
-        ]);
-    }
-
-    public function data()
+    public function render()
     {
         $query = SalesTarget::query()
             ->whereRaw('AchievedSalesTarget >= SalesTarget');
     
         if ($this->start && $this->end) {
-            $query->whereBetween('created_at', [$this->start, $this->end]);
+            $query->whereBetween('Deadline', [$this->start, $this->end]);
         }
     
         if ($this->search) {
-            $query->whereHas('user', function ($subquery) {
-                $subquery->where('name', 'like', '%' . $this->search . '%');
-            });
+            $searchTerm = '%' . $this->search . '%';
+            $query->whereLike([
+                'user.name', 'Deadline'
+            ], $searchTerm);
         }
     
-        return $query->paginate($this->perPage);
+        $data = $query->paginate($this->perPage);
+    
+        return view('livewire.target.index', [
+            'targets' => $data
+        ]);
     }
+    
     
     
 }
