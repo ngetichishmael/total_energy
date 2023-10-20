a0d6fe1b422edee36d2c3b884da6c3714272fb7e
steven-maina <stevenmaina17@gmail.com>
2023-10-20 21:10:28 +0300

    new sales update

--- app/Http/Controllers/Api/CheckingSaleOrderController.php
+++ app/Http/Controllers/Api/CheckingSaleOrderController.php
@@ -128,64 +128,142 @@ class CheckingSaleOrderController extends Controller
         $user_code = $request->user()->user_code;
         $request = $request->collect();
         $total = 0;
-        foreach ($request as $value) {
-            $price_total = $value["qty"] * $value["price"];
-            $total += $price_total;
-            $product = product_information::whereId($value["productID"])->first();
-            Cart::updateOrCreate(
+
+       foreach ($request1 as $value) {
+          if (empty($value)) {
+             continue;
+          }
+
+          $qty = $value["qty"];
+          $price = $value["price"];
+          $productID = $value["productID"];
+
+          $product = product_information::find($productID);
+
+          if ($product) {
+             $price_total = $qty * $price;
+             $total += $price_total;
+
+             // Create or update the cart
+             Cart::updateOrCreate(
                 [
-                    'checkin_code' => Str::random(20),
-                    "order_code" => $random,
+                   'checkin_code' => Str::random(20),
+                   "order_code" => $ordercode,
                 ],
                 [
-                    'productID' => $value["productID"],
-                    "product_name" => $product->product_name,
-                    "qty" => $value["qty"],
-                    "price" => $value["price"],
-                    "amount" => $value["qty"] * $value["price"],
-                    "total_amount" => $value["qty"] * $value["price"],
-                    "userID" => $user_code,
+                   'productID' => $productID,
+                   "product_name" => $product->product_name,
+                   "qty" => $qty,
+                   "price" => $price,
+                   "amount" => $price_total,
+                   "total_amount" => $price_total,
+                   "userID" => $user_code,
                 ]
-            );
-            Order::updateOrCreate(
+             );
+
+             // Create or update the order
+             Order::updateOrCreate(
                 [
-                    'order_code' => $random,
+                   'order_code' => $ordercode,
                 ],
                 [
-                    'user_code' => $user_code,
-                    'customerID' => $checkinCode,
-                    'price_total' => $total,
-                    'balance' => $total,
-                    'order_status' => 'Pending Delivery',
-                    'payment_status' => 'Pending Payment',
-                    'qty' => $value["qty"],
-                    'discount' => $product->discount ?? "0",
-                    'checkin_code' => $checkinCode,
-                    'order_type' => 'Pre Order',
-                    'delivery_date' => now(),
-                    'business_code' => $user_code,
-                    'updated_at' => now(),
+                   'user_code' => $user_code,
+                   'customerID' => $checkinCode,
+                   'price_total' => $total,
+                   'balance' => $total,
+                   'order_status' => 'Pending Delivery',
+                   'payment_status' => 'Pending Payment',
+                   'qty' => $qty,
+                   'supplierID' => $distributor ?? 1,
+                   'discount' => $value["discount"] ?? 0,
+                   'checkin_code' => $checkinCode,
+                   'order_type' => 'Pre Order',
+                   'delivery_date' => now(),
+                   'business_code' => $user_code,
+                   'updated_at' => now(),
                 ]
-            );
-            Order_items::create([
-                'order_code' => $random,
-                'productID' => $value["productID"],
+             );
+
+             // Create order items
+             Order_items::create([
+                'order_code' => $ordercode,
+                'productID' => $productID,
                 'product_name' => $product->product_name,
-                'quantity' => $value["qty"],
-                'sub_total' => $value["qty"] * $value["price"],
-                'total_amount' => $value["qty"] * $value["price"],
-                'selling_price' => $value["price"],
+                'quantity' => $qty,
+                'sub_total' => $price_total,
+                'total_amount' => $price_total,
+                'selling_price' => $price,
                 'discount' => 0,
                 'taxrate' => 0,
                 'taxvalue' => 0,
                 'created_at' => now(),
                 'updated_at' => now(),
-            ]);
+             ]);
 
-            DB::table('orders_targets')
+             // Update orders_targets
+             DB::table('orders_targets')
                 ->where('user_code', $user_code)
-                ->increment('AchievedOrdersTarget', $value["qty"]);
-        }
+                ->increment('AchievedOrdersTarget', $qty);
+          }
+       }
+//        foreach ($request as $value) {
+//            $price_total = $value["qty"] * $value["price"];
+//            $total += $price_total;
+//            $product = product_information::whereId($value["productID"])->first();
+//            Cart::updateOrCreate(
+//                [
+//                    'checkin_code' => Str::random(20),
+//                    "order_code" => $random,
+//                ],
+//                [
+//                    'productID' => $value["productID"],
+//                    "product_name" => $product->product_name,
+//                    "qty" => $value["qty"],
+//                    "price" => $value["price"],
+//                    "amount" => $value["qty"] * $value["price"],
+//                    "total_amount" => $value["qty"] * $value["price"],
+//                    "userID" => $user_code,
+//                ]
+//            );
+//            Order::updateOrCreate(
+//                [
+//                    'order_code' => $random,
+//                ],
+//                [
+//                    'user_code' => $user_code,
+//                    'customerID' => $checkinCode,
+//                    'price_total' => $total,
+//                    'balance' => $total,
+//                    'order_status' => 'Pending Delivery',
+//                    'payment_status' => 'Pending Payment',
+//                    'qty' => $value["qty"],
+//                    'discount' => $product->discount ?? "0",
+//                    'checkin_code' => $checkinCode,
+//                    'order_type' => 'Pre Order',
+//                    'delivery_date' => now(),
+//                    'business_code' => $user_code,
+//                    'updated_at' => now(),
+//                ]
+//            );
+//            Order_items::create([
+//                'order_code' => $random,
+//                'productID' => $value["productID"],
+//                'product_name' => $product->product_name,
+//                'quantity' => $value["qty"],
+//                'sub_total' => $value["qty"] * $value["price"],
+//                'total_amount' => $value["qty"] * $value["price"],
+//                'selling_price' => $value["price"],
+//                'discount' => 0,
+//                'taxrate' => 0,
+//                'taxvalue' => 0,
+//                'created_at' => now(),
+//                'updated_at' => now(),
+//            ]);
+//
+//            DB::table('orders_targets')
+//                ->where('user_code', $user_code)
+//                ->increment('AchievedOrdersTarget', $value["qty"]);
+//        }
        $customer = customers::find($checkinCode);
 
        if ($customer) {
